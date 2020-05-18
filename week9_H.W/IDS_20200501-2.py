aafbec2006bb50e7dfea0ef7e5496151a87da113
hjkyuit1226 <61676781+hjkyuit1226@users.noreply.github.com>
2020-05-19 00:18:39 +0800

    Add files via upload

---
+++ week9_H.W/IDS_20200501-2.py
@@ -0,0 +1,13 @@
+x = int(input("請輸入起始的整數:"))
+y = int(input("請輸入終止的整數:"))
+odd_counter = 0 # 歸零
+i = x # start
+while i <= y: # stop
+    # task
+    if i % 2 == 1:
+        #odd_counter = odd_counter + 1 # = NOT ==
+        odd_counter += 1 # 計數累計
+    #print(odd_counter)
+    i += 1 # step
+print("======")
+print(odd_counter)
